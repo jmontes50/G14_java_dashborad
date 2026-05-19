fd00d2e0ff79850abc992e666be88a84bf6ee32d
Jorge Montesinos <osmarmontes50@gmail.com>
2026-05-18 21:44:51 -0500

    advance

--- src/store/useAuthStore.jsx
+++ src/store/useAuthStore.jsx
@@ -1,7 +1,8 @@
 import { create } from "zustand";
-import { devtools } from "zustand/middleware"
+import { devtools, persist } from "zustand/middleware"
 import client from "../api/client";
 /*
+//versión simple sin persistencia
 const useAuthStore = create((set) => ({
   //que vamos a guardar, como un estado inicial y que acciones podemos realizar, todo junto
   //store - estado
@@ -15,8 +16,10 @@ const useAuthStore = create((set) => ({
     set({ user: res.data.user, token: res.data.token })
   }
 }))
-  */
+*/
 
+/*
+//versión para desarrollo con devtools
 const useAuthStore = create(devtools((set) => ({
   //que vamos a guardar, como un estado inicial y que acciones podemos realizar, todo junto
   //store - estado
@@ -30,5 +33,22 @@ const useAuthStore = create(devtools((set) => ({
     set({ user: res.data.user, token: res.data.token }, false, 'auth/login')
   }
 })))
+*/
+
+const useAuthStore = create(
+  //persist va a hacer que el estado se guarde en localStorage
+  persist((set) => ({
+    //que vamos a guardar, como un estado inicial y que acciones podemos realizar, todo junto
+    //store - estado
+    user: null,
+    token: null,
+
+    //acciones
+    login: async (email, password) => {
+      const res = await client.post('/auth/login', { email, password })
+      console.log({ res });
+      set({ user: res.data.user, token: res.data.token })
+    }
+  }), { name: 'auth-storage' }))
 
 export default useAuthStore;
